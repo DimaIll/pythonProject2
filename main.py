703f041c07b611a382c0c2248f490055e98eae35
DM <xlxdimaxlx@mail.ru>
2023-04-26 18:43:59 +0300

    edited_04

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 candidates = utils.load_candidates()
 
+
 @app.route("/")
 def page_index():
     str_candidates = "<pre>"
@@ -13,20 +14,26 @@ def page_index():
     str_candidates += "<pre>"
     return str_candidates
 
-@app.route("/candidates/<int:id>")
-def profile(id):
-    candidate = candidates[id]
+
+@app.route("/candidates/<int:_id>")
+def profile(_id):
+    candidate = candidates[_id]
+    str_candidates = "<pre>"
     str_candidates += f"{candidate['picture']}\n\n{candidate['position']}\n{candidate['skills']}\n"
-    return candidate
+    str_candidates += "<pre>"
+    return str_candidates
+
 
 @app.route("/skills/<skill>")
 def skills(skill):
     str_candidate = "<pre>"
     for candidate in candidates.values():
-        candidate_skills = candidates["skills"].split(", ")
+        candidate_skills = candidate["skills"].split(", ")
+        candidate_skills = [x.lower() for x in candidate_skills]
         if skill in candidate_skills:
             str_candidate += f"{candidate['name']}\n\n{candidate['position']}\n{candidate['skills']}\n\n"
     str_candidate += "<pre>"
     return str_candidate
+
 
 app.run()
